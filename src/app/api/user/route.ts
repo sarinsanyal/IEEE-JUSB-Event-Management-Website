e6be3c6324dba81d8f755a1c60a2100850120313
sarinsanyal <sarinsanyal2005@gmail.com>
2025-04-02 23:07:43 +0530

    One f change

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,6 @@
 //Dashboard Endpoint
 
-import {NextRequest, NextResponse} from 'next/server';
+import { NextResponse} from 'next/server';
 import connectToDatabase from '@/lib/mongodb';
 import User from "@/models/user";
 import jwt from 'jsonwebtoken';
