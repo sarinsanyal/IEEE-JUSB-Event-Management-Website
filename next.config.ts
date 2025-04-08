500bf688a345a452d03f1d1dad44a70a1c9e26e8
sarinsanyal <sarinsanyal2005@gmail.com>
2025-04-08 19:23:47 +0530

    Just an experiment

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,5 @@ const nextConfig: NextConfig = {
     allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   },
 };
-nextConfig.typescript = {
-  ignoreBuildErrors: true,
-};
+
 export default nextConfig;
